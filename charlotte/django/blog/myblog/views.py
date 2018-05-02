086f57ee93ceeec4b182d9cc3419af60ad839963
balowt <siyarlowt@gmail.com>
2018-05-02 17:54:10 +0800

    update views

--- charlotte/django/blog/myblog/views.py
+++ charlotte/django/blog/myblog/views.py
@@ -8,6 +8,7 @@ from .models import Post, Tag, Category, Comment
 from rest_framework import permissions
 # Create your views here.
 class PostViewSet(viewsets.ViewSet):
+
     def list(self, request):
         queryset = Post.objects.all()
         serializer = PostSerializer(queryset, many=True)
@@ -19,19 +20,37 @@ class PostViewSet(viewsets.ViewSet):
         serializer = PostSerializer(post)
         return Response(serializer.data)
 
+    def create(self, request, pk=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
+         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, format=None):
+        post = self.get_object_or_404(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data)
+         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class TagViewSet(viewsets.ViewSet):
+
     def list(self, request):
         queryset = Tag.objects.all()
         serializer = TagSerializer(queryset, many=True)
         return Response(serializer.data)
 
 class CategoryViewSet(viewsets.ViewSet):
+
     def list(self, request):
         queryset = Category.objects.all()
         serializer = CategorySerializer(queryset, many=True)
         return Response(serializer.data)
 
 class CommentViewSet(viewsets.ViewSet):
+    
     def list(self, request):
         queryset = Comment.objects.all()
         serializer = CommentSerializer(queryset, many=True)
